2a493603c08b9953cb21af061fceb481ec527a01
Santosh Addanki <santosh.kumar.addanki@sap.com>
2019-02-02 19:58:20 -0800

    Check in the source code

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "aws-lambda-scala"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
+
+libraryDependencies ++= Seq(
+  "com.amazonaws" % "aws-lambda-java-core" % "1.1.0"
+)
+        
---
+++ src/main/scala/LambdaApp.scala
@@ -0,0 +1,59 @@
+package example
+
+import scala.collection.JavaConverters._
+
+/***
+  * AWS Lambda interface for Java
+  */
+trait LambdaApp {
+
+  /***
+    * Handlers
+    */
+  def handler(): java.util.List[String]
+
+  def cleanUp() = {}
+
+  /***
+    * Driver for testing handler locally
+    */
+  def main(args: Array[String]): Unit = {
+    val uuid = java.util.UUID.randomUUID
+    val version = "$LATEST"
+
+    println(s"START RequestId: $uuid Version: $version")
+
+    val t0 = System.nanoTime
+    var result: java.util.List[String] = new java.util.ArrayList[String]()
+    try {
+      result = handler()
+    } catch {
+      case e: Throwable => e.printStackTrace
+    } finally {
+      cleanUp()
+    }
+    println(s"END RequestId: $uuid")
+
+    val t1 = System.nanoTime
+    val duration = (t1 - t0) / 1e6
+    val billedDuration = scala.math.ceil(duration / 100d).toLong * 100
+
+    val env = Runtime.getRuntime
+    val memorySize = env.totalMemory / 1024 / 1024
+    val memoryUsed = (env.totalMemory - env.freeMemory) / 1024 / 1024
+
+    println(s"""REPORT RequestId: $uuid
+               |  Duration: ${duration} ms
+               |  Billed Duration: ${billedDuration} ms
+               |  Memory Size: ${memorySize} MB
+               |  Max Memory Used: ${memoryUsed} MB""".stripMargin
+      .replaceAll("\n", ""))
+
+    println("[")
+    println("  " + result.asScala.map(escString(_)).mkString(",\n  "))
+    println("]")
+  }
+
+  def escString(s: String) =
+    "\"" + s.replaceAll("\"", "\\\"") + "\""
+}
